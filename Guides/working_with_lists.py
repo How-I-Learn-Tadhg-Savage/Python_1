bb8a04e5a5e6ec842cdbd2ee7e34dc02205e49f8
tadhgsavageplymouth <tadhg.savage@students.plymouth.ac.uk>
2025-04-15 19:28:33 +0100

    Additions to working_with_lists Guides

--- Guides/working_with_lists.py
+++ Guides/working_with_lists.py
@@ -76,6 +76,66 @@ print (digits)
 squares = [value ** 2 for value in range(1, 11)]
 print (squares)
 
+# Slicing a list in python
+players = ['chales', 'phobe', 'katie', 'chiara', 'tadhg', 'dan', 'jason', 'anne', 'mat']
+print (players[0:3])
+# One can slice at sny intervel set
+print (players[1:4])
+# If you omit the first index python starts at the begginging of the list
+print (players[:3])
+# If you omit the last index python will print to the end of the list
+print (players[2:])
+# Remeber we can also use a negative index to output begining or end index of a list regarless of length
+# This will only print the last three values in a list
+print (players[-3:])
+# This will always omit the last three values in a list
+print (players[:-3])
+
+# We can now use this to show how we can loop through a slice to print a message
+# Defining a list
+players = ['dan', 'jason', 'anne', 'tadhg', 'caoimhe']
+# Initial print statement outside of loop
+print ("Welcome to the game:")
+# Creating the for loop for certain index range
+for player in players[:3]:
+    # Printing inside of the for loop each player to title
+    print(player.title())
+
+
+# Copying a list 
+# We can use slices to copy a list by taking a slice of an entire list 
+cars = ['bugatti', 'bmw', 'mercedes', 'jaguar', 'ford']
+# We can copy to a new list by taking a slice using no indexs to get a copy of an entire list
+friends_cars = cars[:]
+print ("My favourite cars are:")
+print (friends_cars)
+# A new copy has been made and to prove we can append new items to both in the list
+cars.append("kia")
+friends_cars.append("honda")
+print (f'\n{cars}')
+print (friends_cars)
+
+# Now you cannot copy a list by setting a list equal to a new list, foir example
+my_foods = ['beans', 'toast', 'eggs', 'bacon', 'beef']
+# THIS WILL NOT WORK (LOGICAL ERROR)
+# This has set friends_foods equal to my_foods
+friends_foods = my_foods
+print (my_foods)
+print (friends_foods)
+# Appending a new different item to both lists
+my_foods.append('turkey')
+friends_foods.append('yogurt')
+# Printing both lists and they will now contain oneanothers food, logical error produced
+print (my_foods)
+print (friends_foods)
+
+
+
+
+
+
+
+
 
 
 
