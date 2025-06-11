5e04feedbbbd5abd36478eda1b925b8cb01ce8b3
tadhgsavageplymouth <tadhg.savage@students.plymouth.ac.uk>
2025-06-11 13:56:28 +0100

    Additions to if_statements guide

--- Guides/if_statements.py
+++ Guides/if_statements.py
@@ -11,3 +11,24 @@ for car in cars:
     else:
         print(car.title())
 
+# Set the value of car equal to 'audi'
+car = 'audi'
+# Is the value of car equal to 'Audi'?
+car == 'Audi'
+# Case sensitivity matter to change variable to title
+car.title() == 'Audi'
+
+# Checking for inequality 
+requested_topping = 'mushrooms'
+if requested_topping != 'anchovies':
+    print('Hold the anchovies!')
+else:
+    print('Use anchovies')
+
+# Numerical comparison 
+age = 22
+if age >= 18:
+    print('True')
+
+
+
